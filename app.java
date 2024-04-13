badf6fdc6494cb4276ecafe7e2384702202f2e51
Steven <steven277015@gmail.com>
2024-04-13 13:28:34 -0500

    Board

--- app.java
+++ app.java
@@ -1,1 +1,15 @@
+import javax.swin.*;
 
+public class APP{
+    public static void main(string[] args) throws Exception{
+        int boardwidth = 600;
+        int boardheight = boardwidth;
+
+        JFrame frame = new Jframe("snake");
+        frame.setVisible(true);
+        frame.setSize(boardwidth,boardheight);
+        frame.setLocationRelativeto(null);
+        frame.setResizeable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
