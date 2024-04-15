fe7f65f9e437b61a1b18989127683e579297b5bf
Steven <steven277015@gmail.com>
2024-04-15 04:11:31 -0500

    Made Basic SnakeGame

--- app.java
+++ app.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
 
-public class APP{
-    public static void main(string[] args) throws Exception{
+public class app {
+    public static void main(String[] args) throws Exception{
         int boardwidth = 600;
         int boardheight = boardwidth;
 
-        JFrame frame = new Jframe("snake");
+        JFrame frame = new JFrame("snake");
         frame.setVisible(true);
         frame.setSize(boardwidth,boardheight);
-        frame.setLocationRelativeto(null);
-        frame.setResizeable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardwidth, boardheight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
     }
 }
